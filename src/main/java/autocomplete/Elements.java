96afd4d33818d428602c6c5005e6e284578b3146
kevstevie <jianna442@gmail.com>
2024-05-24 17:35:32 +0900

    style: 컨벤션 수정

--- src/main/java/autocomplete/Elements.java
+++ src/main/java/autocomplete/Elements.java
@@ -4,27 +4,27 @@ import java.util.List;
 
 public class Elements {
 
-    private final List<String> FIRST_CONSONANTS = List.of(
+    private final List<String> firstConsonants = List.of(
             "ㄱ", "ㄲ", "ㄴ", "ㄷ", "ㄸ", "ㄹ", "ㅁ", "ㅂ",
             "ㅃ", "ㅅ", "ㅆ", "ㅇ", "ㅈ", "ㅉ", "ㅊ", "ㅋ", "ㅌ", "ㅍ", "ㅎ");
 
-    private final List<String> MIDDLE_VOWELS = List.of(
+    private final List<String> middleVowels = List.of(
             "ㅏ", "ㅐ", "ㅑ", "ㅒ", "ㅓ", "ㅔ", "ㅕ", "ㅖ",
             "ㅗ", "ㅘ", "ㅙ", "ㅚ", "ㅛ", "ㅜ", "ㅝ", "ㅞ", "ㅟ", "ㅠ", "ㅡ", "ㅢ", "ㅣ");
 
-    private final List<String> LAST_CONSONANTS = List.of("", "ㄱ", "ㄲ", "ㄳ", "ㄴ", "ㄵ", "ㄶ", "ㄷ",
+    private final List<String> lastConsonants = List.of("", "ㄱ", "ㄲ", "ㄳ", "ㄴ", "ㄵ", "ㄶ", "ㄷ",
             "ㄹ", "ㄺ", "ㄻ", "ㄼ", "ㄽ", "ㄾ", "ㄿ", "ㅀ", "ㅁ", "ㅂ",
             "ㅄ", "ㅅ", "ㅆ", "ㅇ", "ㅈ", "ㅊ", "ㅋ", "ㅌ", "ㅍ", "ㅎ");
 
     public String getFirstConsonantAt(final int index) {
-        return FIRST_CONSONANTS.get(index);
+        return firstConsonants.get(index);
     }
 
     public String getMiddleVowelAt(final int index) {
-        return MIDDLE_VOWELS.get(index);
+        return middleVowels.get(index);
     }
 
     public String getLastConsonantAt(final int index) {
-        return LAST_CONSONANTS.get(index);
+        return lastConsonants.get(index);
     }
 }
